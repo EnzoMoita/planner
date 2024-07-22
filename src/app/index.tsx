15efbd515033b26ef69fe06fbc8518de592c91f4
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-22 20:47:33 -0300

    InicialProjectConfigs

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text} from 'react-native'
+
+export default function Index() {
+    return (
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-zinc-400'>Covide seus amigos e planeje sua próxima viagem</Text>
+        </View>
+   
+    )
+}
