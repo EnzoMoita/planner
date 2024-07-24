3afe7570844b35a5842d37e021bcff221b92c7bd
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-23 21:10:18 -0300

    FinishProject

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,28 +1,37 @@
-import '@/styles/global.css'
-import { Slot } from 'expo-router'
-import { View, StatusBar } from 'react-native'
-import { Loading } from '@/components/loading'
-import { useFonts, Inter_500Medium, Inter_400Regular, Inter_600SemiBold} from '@expo-google-fonts/inter'
+import "@/styles/global.css"
+import "@/utils/dayjsLocaleConfig"
 
-export default function Layout(){
-    const [fontsLoaded] = useFonts({
-        Inter_500Medium,
-        Inter_400Regular,
-        Inter_600SemiBold
-    })
+import { View, StatusBar } from "react-native"
+import { Slot } from "expo-router"
 
-    if(!fontsLoaded) {
-        return  <Loading />
-    }
+import {
+  useFonts,
+  Inter_500Medium,
+  Inter_400Regular,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter"
 
-    return (
-    <View className='flex-1 bg-zinc-950'>
-        <StatusBar
+import { Loading } from "@/components/loading"
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+    Inter_600SemiBold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <View className="flex-1 bg-zinc-950">
+      <StatusBar
         barStyle="light-content"
-        backgroundColor={'transparent'}
+        backgroundColor="transparent"
         translucent
-        />
-        <Slot />
+      />
+      <Slot />
     </View>
-    )
+  )
 }
